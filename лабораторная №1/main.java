899ce089692df8e322b2eb82787b1eded64e9fbf
Nikola367 <144092182+Nikola367@users.noreply.github.com>
2025-10-31 11:33:08 +0300

    Update main.java

--- лабораторная №1/main.java
+++ лабораторная №1/main.java
@@ -1,1 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+        System.setOut(new PrintStream(System.out, true, StandardCharsets.UTF_8));
+        try {
+            System.out.println("загрузка из JSON-файла");
+            String jsonFilePath = "C:/Users/user/Desktop/customers.json";
+            Customer customerFromJson = new Customer(jsonFilePath, true);
+            printCustomer(customerFromJson);
+            System.out.println("\nсоздание через обычный конструктор");
+            Customer customerFromParams = new Customer(
+                    "ООО Василёк",
+                    "г. Екатеринбург, ул. Ленина, д. 7",
+                    "+7 912 333-44-55",
+                    "Петров Сергей Николаевич"
+            );
+            printCustomer(customerFromParams);
+            System.out.println("\nсоздание из строки (CSV-формат)");
+            String dataString = "ООО Тюльпан;г. Новосибирск, ул. Кирова, д. 3;8 (913) 222-33-44;Смирнов Алексей Иванович";
+            Customer customerFromString = new Customer(dataString);
+            printCustomer(customerFromString);
 
+        } catch (Exception e) {
+            System.err.println("Ошибка: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+        private static void printCustomer(Customer customer) {
+        System.out.println("Имя: " + customer.getName());
+        System.out.println("Адрес: " + customer.getAddress());
+        System.out.println("Телефон: " + customer.getPhone());
+        System.out.println("Контактное лицо: " + customer.getContactPerson());
+    }
+}
